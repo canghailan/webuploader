653efbeae97f8469c11932a8ed796d6b5a1356ea
huhao <415328529@qq.com>
2016-10-10 13:52:06 +0800

    阿里云OSS支持

--- dist/webuploader.custom.js
+++ dist/webuploader.custom.js
@@ -3280,6 +3280,8 @@
     
         Transport.options = {
             server: '',
+            // 阿里云OSS直传授权接口
+            ossPostPolicyServer: 'http://www.yitong.com/apis/aliyun-oss-file-service/v1/oss/post-policy.jsonp',
             method: 'POST',
     
             // 跨域时，是否允许携带cookie, 只有html5 runtime才有效
@@ -3330,8 +3332,35 @@
             },
     
             send: function( method ) {
-                this.exec( 'send', method );
-                this._timeout();
+                // this.exec( 'send', method );
+                // this._timeout();
+                // 上传前先获取阿里云OSS授权，考虑兼容性，使用jsonp
+                var me = this,
+                    opts = me.options;
+            
+                var object = '/' + opts.filename;
+                var callback = 'webuploader_callback_' + new Date().getTime();
+                window[callback] = function(ossPostPolicy) {
+                    me._ossPostPolicy = ossPostPolicy;
+                    me.options.server = ossPostPolicy.server;
+                    me._formData[ 'OSSAccessKeyId' ] = ossPostPolicy[ 'OSSAccessKeyId' ];
+                    me._formData[ 'policy' ] = ossPostPolicy[ 'policy' ];
+                    me._formData[ 'Signature' ] = ossPostPolicy[ 'Signature' ];
+                    me._formData[ 'key' ] = ossPostPolicy[ 'key' ];
+                    me._formData[ 'success_action_status' ] = ossPostPolicy[ 'success_action_status' ];
+                    me.exec( 'send', method );
+                    me._timeout();
+    
+                    try {
+                        window[callback] = undefined;
+                        delete window[callback];
+                    } catch(e) {
+                    }
+                };
+    
+                var script = document.createElement('script');
+                script.src = opts.ossPostPolicyServer + '?callback=' + callback + '&object=' + encodeURIComponent(object);
+                document.body.appendChild(script);
             },
     
             abort: function() {
@@ -3347,11 +3376,37 @@
             },
     
             getResponse: function() {
-                return this.exec('getResponse');
+                // return this.exec('getResponse');
+                // 统一getResponse与getResponseAsJson结果
+                var response = this.getResponseAsJson();
+                if (window.JSON) {
+                    return JSON.stringify(response);
+                }
+                // 兼容
+                var keyValues = [];
+                for (var key in response) {
+                    var value = response[key];
+                    if (typeof value == "number") {
+                        keyValues.push('"' + key + '":' + value);
+                    } else {
+                        keyValues.push('"' + key + '":"' + value + '"');
+                    }
+                }
+                return '{' + keyValues.join(',') + '}';
             },
     
             getResponseAsJson: function() {
-                return this.exec('getResponseAsJson');
+                // return this.exec('getResponseAsJson');
+                // 阿里云OSS 200状态码返回值为空，使用授权接口信息
+                return {
+                    name: this._formData.name,
+                    size: this._formData.size,
+                    type: this._formData.type,
+                    lastModifiedDate: this._formData.lastModifiedDate,
+                    id: this._formData.id,
+                    state: 'SUCCESS',
+                    url: this._ossPostPolicy.url
+                };
             },
     
             getStatus: function() {
